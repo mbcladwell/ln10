9a7659147c18718e8d733282b85e4f2772ab8b54
mbcladwell <mbcladwell@gmail.com>
2021-11-11 10:33:18 -0500

    modified install-lnpg.sh

--- install-lnpg.sh
+++ install-lnpg.sh
@@ -4,13 +4,10 @@ source /home/admin/.guix-profile/etc/profile
     export LC_ALL="C"
   
     sudo chmod -R a=rwx /home/admin/ln10
-
-pg_ctl -D /var/lib/postgresql/11/main -l /var/log/postgresql/postgresql-11-main.log stop
-
-    sudo sed -i 's/host[ ]*all[ ]*all[ ]*127.0.0.1\/32[ ]*md5/host    all        all             127.0.0.1\/32        trust/' /var/lib/postgresql/11/main/pg_hba.conf
-    sudo sed -i 's/\#listen_addresses =/listen_addresses =/'  /var/lib/postgresql/11/main/postgresql.conf
-
-pg_ctl -D /var/lib/postgresql/11/main -l /var/log/postgresql/postgresql-11-main.log start
+    sudo pg_ctl -D /var/lib/postgresql/11/main -l /var/log/postgresql/postgresql-11-main.log stop
+    sudo sed -i 's/host[ ]*all[ ]*all[ ]*127.0.0.1\/32[ ]*md5/host    all        all             127.0.0.1\/32        trust/' /etc/postgresql/11/main/pg_hba.conf
+    sudo sed -i 's/\#listen_addresses =/listen_addresses =/'  /etc/postgresql/11/main/postgresql.conf
+    sudo pg_ctl -D /var/lib/postgresql/11/main -l /var/log/postgresql/postgresql-11-main.log start
     
     psql -U admin -h 127.0.0.1 postgres -a -f /home/admin/ln10/initdba.sql
     psql -U admin -h 127.0.0.1 lndb -a -f /home/admin/ln10/initdbb.sql
